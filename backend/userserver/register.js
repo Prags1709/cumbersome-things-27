b795eea599aa86282bafe34b920551850ed387a7
Birendra <divyabiru27@gmail.com>
2023-02-26 20:12:41 +0530

    h

--- backend/userserver/register.js
+++ backend/userserver/register.js
@@ -3,6 +3,7 @@ const cookieParser = require('cookie-parser')
 const cors = require('cors')
 const app=express();
 app.use(express.json());
+
 app.use(cookieParser())
 app.use(cors())
 const {connection}=require("./config/config")
