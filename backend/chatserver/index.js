0930fcd36f0a517256c0ed28a96d199bb02a007c
Birendra <divyabiru27@gmail.com>
2023-04-07 12:30:59 +0530

    able to send message

--- backend/chatserver/index.js
+++ backend/chatserver/index.js
@@ -23,7 +23,6 @@ app.get("/channel",async (req,res)=>{
     let query = req.query;
     try {
         let data = await ChatModel.find(query)
-        // console.log(query.channel);
         res.send(data)
     } catch (error) {
         console.log(error);
@@ -36,42 +35,20 @@ const botename = "We Connect"
 io.on("connection",(socket)=>{
 
     console.log("one client joined");
-   // console.log(socket.id);
-
-    // socket.emit("welcome",formateMessage(botename, "Welcome to we connect"))
-
     socket.on("user_channel",({username,channel})=>{
-        console.log(username,channel);
+       
         socket.join(channel)
         socket.emit("welcome",formateMessage(botename, "Welcome to we connect"))
 
         socket.on("chatMessage",async (text)=>{
-            console.log(text);
-            
-           // socket.join(channel)
-            console.log(channel);
             io.to(channel).emit("message_all",formateMessage(username,text))
-            // try {
-            //     let data = new ChatModel({
-            //         channel_name:channel,
-            //         name:username,
-            //         message:text,
-            //         time:moment().format('DD MM YYYY h:mm a')
-            //     })
-            //     await data.save()
-            // } catch (error) {
-            //     console.log(error);
-            // }
+            
         })
     })
 })
 
 const PORT = 8081;
 server.listen(PORT,async ()=>{
-    // try {
-    //     await connection;   
-    // } catch (error) {
-    //     console.log(error)
-    // }
+    
     console.log(`server running on port ${PORT}`);
 })
